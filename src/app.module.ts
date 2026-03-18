9e144820ae1320a45fd66b91876967f24a0c23d6
Thanh Loi <marcomoi395@gmail.com>
2026-03-18 14:13:17 +0700

    feat: set global API prefix, and enable CORS

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,8 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { typeOrmOptions } from './database/typeorm.config';
 
 @Module({
-    imports: [TypeOrmModule.forRoot(typeOrmOptions)],
-    controllers: [AppController],
-    providers: [AppService],
+    imports: [TypeOrmModule.forRoot(typeOrmOptions), AuthModule, GiftsModule, UsersModule],
 })
 export class AppModule {}
