66b1faa127ca9948186fc030e10d0c08c300d999
Thanh Loi <marcomoi395@gmail.com>
2026-03-18 15:02:07 +0700

    feat: enhance authentication with validation and update guards

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -6,7 +6,6 @@ import type { StringValue } from 'ms';
 import { User } from '../database/entities/user.entity';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
-import { JwtAuthGuard } from './guards/jwt-auth.guard';
 import { RolesGuard } from './guards/roles.guard';
 import { JwtStrategy } from './strategies/jwt.strategy';
 
@@ -22,7 +21,7 @@ import { JwtStrategy } from './strategies/jwt.strategy';
         }),
     ],
     controllers: [AuthController],
-    providers: [AuthService, JwtStrategy, JwtAuthGuard, RolesGuard],
-    exports: [AuthService, JwtAuthGuard, RolesGuard],
+    providers: [AuthService, JwtStrategy, RolesGuard],
+    exports: [AuthService, RolesGuard],
 })
 export class AuthModule {}
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,16 +1,16 @@
 import { Controller, Get, Req, UseGuards } from '@nestjs/common';
 import type { Request } from 'express';
-import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
 import type { AuthenticatedUser } from '../auth/strategies/jwt.strategy';
 import { User } from '../database/entities/user.entity';
 import { UsersService } from './user.service';
+import { AuthGuard } from '@nestjs/passport';
 
 @Controller('user')
 export class UsersController {
     constructor(private readonly usersService: UsersService) {}
 
     @Get('me')
-    @UseGuards(JwtAuthGuard)
+    @UseGuards(AuthGuard('jwt'))
     me(@Req() req: Request & { user: AuthenticatedUser }): Promise<User> {
         return this.usersService.getCurrentUser(req.user.userId);
     }
